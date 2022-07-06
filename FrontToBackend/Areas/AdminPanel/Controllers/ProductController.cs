a2cc163e7fe15abd45feab46b5f225c37f8e40ee
SahilMamedov <sahilim@code.edu.az>
2022-07-06 13:55:31 +0400

    feat: product update

--- FrontToBackend/Areas/AdminPanel/Controllers/ProductController.cs
+++ FrontToBackend/Areas/AdminPanel/Controllers/ProductController.cs
@@ -101,5 +101,65 @@ namespace FrontToBackend.Areas.AdminPanel.Controllers
 
             return RedirectToAction("Index");
         }
+
+
+
+
+        public async Task<IActionResult> Update(int? id)
+        {
+
+            ViewBag.Categories = _context.categories.ToList();
+
+            if (id == null) return NotFound();
+            Product dbProduct = await _context.Products.FindAsync(id);
+            if (dbProduct == null) return NotFound();
+
+
+            return View(dbProduct);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Product product)
+        {
+
+            ViewBag.Categories = _context.categories.ToList();
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            Product dbProduct = _context.Products.FirstOrDefault(c => c.id == product.id);
+
+
+            if (product.Photo == null)
+            {
+                ModelState.AddModelError("Photo", "bosh olmaz");
+
+                return View();
+            }
+           
+            if (!product.Photo.IsImage())
+            {
+                ModelState.AddModelError("Photo", "yalniz shekil olar");
+                return View();
+            }
+            if (product.Photo.ValidSize(500))
+            {
+                ModelState.AddModelError("Photo", "olcunu duzgun verin");
+                return View();
+            }
+
+            string path = Path.Combine(_env.WebRootPath, "img", dbProduct.imgUrl);
+            System.IO.File.Delete(path);
+
+
+            dbProduct.Photo = product.Photo;
+            dbProduct.Name = product.Name;
+            dbProduct.imgUrl = product.Photo.SaveImage(_env, "img");
+            dbProduct.CategoryId = product.CategoryId;
+            _context.SaveChanges();
+
+            return RedirectToAction("index");
+        }
     }
 }
